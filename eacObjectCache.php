40d9bd18565ebf9596c4f2323881788aa9c52ee7
Kevin Burkholder <149023389+KBurkholder@users.noreply.github.com>
2024-05-25 07:43:19 -0400

    Version 1.0.4
    = Version 1.0.4 - May 24, 2024 =
    
    +   Fix for updated {eac}Doojigger Advanced Mode, remove from 'Tools' tab.

--- eacObjectCache.php
+++ eacObjectCache.php
@@ -14,7 +14,7 @@ namespace EarthAsylumConsulting;
  * @wordpress-plugin
  * Plugin Name:			{eac}ObjectCache
  * Description:			{eac}Doojigger Object Cache - SQLite powered WP_Object_Cache Drop-in
- * Version:				1.0.3
+ * Version:				1.0.4
  * Requires at least:	5.5.0
  * Tested up to:		6.5
  * Requires PHP:		7.4
